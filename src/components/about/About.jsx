a3325684699445ad9c91da836a68dd4fe9a5b6fb
Callahan Hinckley <139603683+callahanhinckley@users.noreply.github.com>
2023-08-28 16:39:32 -1000

    First major committ, moved everything to my folder

---
+++ src/components/about/About.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import './About.css';
+import Image from '../../assets/avatar-2.svg';
+import Resume from '../../assets/resume.pdf';
+import AboutBox from './AboutBox';
+
+const About = () => {
+    const downloadResume = async () => {
+        window.open(Resume, '_blank');
+    }
+
+
+    return (
+        <section className="about container section" id="about">
+            <h2 className="section__title">About Callahan! </h2>
+
+            <div className="about__container grid">
+                <img src={Image} alt="" className='about__img' />
+
+                <div className="about__data grid">
+                    <div className="about__info">
+                        <p className="about__description">
+                            Hello! I'm Callahan, and I'm based in Honolulu, Hawaii. I studied Statistics and Business Administration at The University of North Carolina at Chapel Hill.<br /><br />
+                           I broke into Product Management from a Data Analysis role and can say I honestly love both! I'm fascinated by the intersection of business and data, especially in the startup space. All of my PM experience has been in Fintech, but I'm also interested in consumer-facing products and Artificial Intelligence. <br /><br />
+                            Here are a few technologies I’ve been working with recently:
+                        </p>
+                        <ul className="about__list">
+                            <li>Tableau</li>
+                            <li>Excel</li>
+                            <li>SQL</li>
+                        </ul>
+                        <button className="btn" onClick={downloadResume}>Donwload Resume</button>
+                    </div>
+
+                    {/* <div className="about__skills grid">
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Development</h3>
+                                <span className="skills__number">90%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage development">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">UI/UX Design</h3>
+                                <span className="skills__number">80%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage ui__design">
+
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="skills__data">
+                            <div className="skills__titles">
+                                <h3 className="skills__name">Photography</h3>
+                                <span className="skills__number">60%</span>
+                            </div>
+
+                            <div className="skills__bar">
+                                <span className="skills__percentage photography">
+
+                                </span>
+                            </div>
+                        </div>
+                    </div> */}
+                </div>
+            </div>
+
+            <AboutBox />
+        </section>
+    )
+}
+
+export default About
